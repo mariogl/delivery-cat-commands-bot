8cf57c92b894ee56ece5dae69f122968f3fe22cc
Mario González <mario@mariogl.com>
2022-08-11 10:15:16 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
