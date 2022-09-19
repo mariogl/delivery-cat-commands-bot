865d83a9e7a3bf526d2ee06b307af8a2f4922798
Mario González <mario@mariogl.com>
2022-09-19 18:41:23 +0200

    Add server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
+import http from "http";
 import chalk from "chalk";
 import "./loadEnvironment.js";
 import "./discord";
 import connectDB from "./db/index.js";
 
 (async () => {
-  await connectDB(process.env.MONGODB_CONNECTION).catch((error) => {
-    console.log(
-      chalk.red(`Error al iniciar la base de datos: ${error.message}`)
+  http
+    .createServer((req, res) => {
+      res.write("Hi");
+      res.end();
+    })
+    .listen(process.env.PORT || 8080, () =>
+      console.log(chalk.green("Server up"))
     );
+  await connectDB(process.env.MONGODB_CONNECTION).catch((error) => {
+    console.log(chalk.red(`Error on starting database: ${error.message}`));
     throw new Error();
   });
-  console.log(chalk.blue("Base de datos iniciada"));
+  console.log(chalk.blue("Connected to database"));
 })();
